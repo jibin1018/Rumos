bba343d27ecd03fa51eab83a7abed937184f870d
Desktop <joyonggeun16@gmail.com>
2025-05-20 22:35:47 +0900

    Update df.js & docker-compose.yml

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    database: "rumos", // Changed from "lumos" to "rumos" as mentioned in SQL
-    user: "postgres",
-    password: "1111"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 // Test database connection
